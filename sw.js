adf2d66caedb8af8e84ac1909e8c64a68290b090
rafal <rafal@tymoszuk.com>
2020-05-16 15:19:23 +0200

    offline fix

--- sw.js
+++ sw.js
@@ -2,12 +2,24 @@ const staticCacheName='wersja-statyczna';
 const assets = [
 '/',
 '/index.html',
+'/fallback.html',
 '/js/app.js',
+'/manifest.json',
 '/vendor/fontawesome-free/css/all.min.css',
-'vendor/bootstrap/css/bootstrap.min.css',
+'/vendor/bootstrap/css/bootstrap.min.css',
+'/vendor/jquery/jquery.min.js',
 '/vendor/simple-line-icons/css/simple-line-icons.css',
+'/vendor/simple-line-icons/fonts/Simple-Line-Icons.eot?v=2.4.0',
+'/vendor/simple-line-icons/fonts/Simple-Line-Icons.svg?v=2.4.0',
+'/vendor/simple-line-icons/fonts/Simple-Line-Icons.woff?v=2.4.0',
+'/vendor/simple-line-icons/fonts/Simple-Line-Icons.woff2?v=2.4.0',
+'/vendor/simple-line-icons/fonts/Simple-Line-Icons.ttf?v=2.4.0',
+'/vendor/fontawesome-free/webfonts/fa-brands-400.woff2',
 'https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic',
-'css/landing-page.min.css',
+'/vendor/bootstrap/js/bootstrap.bundle.min.js',
+'/vendor/fontawesome-free/webfonts/fa-brands-400.ttf',
+'/vendor/fontawesome-free/webfonts/fa-brands-400.woff',
+'/css/landing-page.min.css',
 '/img/bg-masthead.jpg',
 '/img/bg-showcase-1.jpg',
 '/img/bg-showcase-2.jpg',
@@ -36,6 +48,6 @@ self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request).then(cacheRes =>{
             return cacheRes || fetch(evt.request);
-        })
+        }).catch(()=> cashes.match('/fallback.html'))
     )
 });
